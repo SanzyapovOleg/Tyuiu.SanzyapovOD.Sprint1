22f69080c0a0a3b2b79e7c893fc15d58a81d39d0
Tyuiu.SanzyapovOD@outlook.com <oleg.sanzjapov@mail.ru>
2024-09-30 11:40:09 +0500

    Task1

---
+++ Tyuiu.SanzyapovOD.Sprint1.Task1.V6/Program.cs
@@ -0,0 +1,37 @@
+﻿using Tyuiu.SanzyapovOD.Sprint1.Task1.V6.Lib;
+namespace Tyuiu.SanzyapovOD.Sprint1.Task1.V6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #1 | Выполнил: Санзяпов О. Д. | ИСТНб-24-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("* Тема: Организация ввода и вывода в консольных приложениях               *");
+            Console.WriteLine("* Задание #1                                                              *");
+            Console.WriteLine("* Вариант #6                                                              *");
+            Console.WriteLine("* Выполнил: Санзяпов Олег Дмитриевич | ИСТНб-24-1                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая вычисляет выражение (x + y) / (3 * y)       *");
+            Console.WriteLine("* и печатает результат на экране.                                         *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            double x, y;
+            Console.WriteLine("Введите значение X:");
+            x = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите значение Y:");
+            y = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine(ds.Calculate(x, y));
+            Console.ReadLine();
+        }
+    }
+    
+}
