0cedb2de1464c593ecfd482059210030c428647a
Tyuiu.SanzyapovOD@outlook.com <oleg.sanzjapov@mail.ru>
2024-10-01 23:19:05 +0500

    Task5

---
+++ Tyuiu.SanzyapovOD.Sprint1.Task5.V6.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+using Tyuiu.SanzyapovOD.Sprint1.Task5.V6.Lib;
+namespace Tyuiu.SanzyapovOD.Sprint1.Task5.V6.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            int k = 20;
+            DataService ds = new DataService();
+            double res = ds.Calculate(k);
+            int result = Convert.ToInt32(res);
+            int wait = 6;
+            Assert.AreEqual(wait, result);
+        }
+    }
+}
---
+++ Tyuiu.SanzyapovOD.Sprint1.Task5.V6/Program.cs
@@ -0,0 +1,34 @@
+﻿using Tyuiu.SanzyapovOD.Sprint1.Task5.V6.Lib;
+namespace Tyuiu.SanzyapovOD.Sprint1.Task5.V6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #1 | Выполнил: Санзяпов О. Д. | ИСТНб-24-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("* Тема: Базовые навки работы в C#                                         *");
+            Console.WriteLine("* Задание #5                                                              *");
+            Console.WriteLine("* Вариант #6                                                              *");
+            Console.WriteLine("* Выполнил: Санзяпов Олег Дмитриевич| ИСТНб-24-1                          *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая запрашивает у пользователя исходные данные, *");
+            Console.WriteLine("* какой день года соответствует какому дню недели.                        *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("Введите день года: ");
+            int k = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            int res = Convert.ToInt32(ds.Calculate(k));
+            Console.WriteLine("День недели: " + res);
+            Console.ReadLine();
+        }
+    }
+}
