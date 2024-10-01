2daefe8fec65c889f32fcb0cbca99e453c2d9958
Tyuiu.SanzyapovOD@outlook.com <oleg.sanzjapov@mail.ru>
2024-10-01 23:09:59 +0500

    Task4

---
+++ Tyuiu.SanzyapovOD.Sprint1.Task4.V19.Lib/DataService.cs
@@ -0,0 +1,15 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint1;
+namespace Tyuiu.SanzyapovOD.Sprint1.Task4.V19.Lib
+{
+    public class DataService : ISprint1Task4V19
+    {
+        public double Calculate(double x, double y)
+        {
+            double value;
+            value = (x + y) / Math.Abs(x - 2);
+            double res;
+            res = Math.Round(value, 3);
+            return res;
+        }
+    }
+}
---
+++ Tyuiu.SanzyapovOD.Sprint1.Task4.V19/Program.cs
@@ -0,0 +1,33 @@
+﻿using Tyuiu.SanzyapovOD.Sprint1.Task4.V19.Lib;
+namespace Tyuiu.SanzyapovOD.Sprint1.Task4.V19
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Спринт #1 | Выполнил: Санзяпов О. Д. | ИСТНб-24-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт 1                                                                *");
+            Console.WriteLine("* Тема: Операторы составного присваивания                                 *");
+            Console.WriteLine("* Задание #4                                                              *");
+            Console.WriteLine("* Вариант #19                                                             *");
+            Console.WriteLine("* Выполнил: Санзяпов Олег Дмитриевич| ИСТНб-24-1                          *");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая запрашивает у пользователя исходные данные, *");
+            Console.WriteLine("* вычисляет результат по формуле и печатает его на экране.                *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            DataService ds = new DataService();
+            Console.WriteLine("Введите x:");
+            double x = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите y:");
+            double y = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("* РЕЗУЛЬТАТ                                                               *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine(ds.Calculate(x, y));
+            Console.ReadKey();
+        }
+    }
+}
